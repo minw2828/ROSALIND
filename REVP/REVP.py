98a00eff6bcf237a3fd6e16acf337be87231c698
Sanyk28 <san-heng-yi-shu@163.com>
2013-10-07 00:17:01 +1100

    modify REVP/REVP.py

--- REVP/REVP.py
+++ REVP/REVP.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+
+
 '''
 Author:
 
@@ -5,55 +8,74 @@ Sanyk28 (san-heng-yi-shu@163.com)
 
 Date created:
 
-2 June 2013
+18 June 2013
 
 Rosalind problem:
 
-Given: A DNA string s of length at most 1000 nt.
+Locating Restriction Sites
 
-Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s.
+Given: A DNA string of length at most 1 kbp in FASTA format
+
+Return: The position and length of every reverse palindrome in the string having length between 4 and 12. 
+You may return these pairs in any order
 
 Usage:
 
-python DNA.py [Input File]
+python REVP.py [Input File]
 
 '''
 
 
-def parse_fasta(fasta):
+def Read_File():
+
+    input_file = sys.argv[-1]
+    f = open(input_file)
+    raw_input = f.readlines()
+    f.close()
+
+    return raw_input
+
+
+def Parse_FASTA(raw_input):
+    
     data = {}
-    for ird in open(fasta).readlines():
-        if ird[0]==">":
-            key = ird[1:].strip()
-            data[key]=""
+    for item in raw_input:
+        if item[0] == '>':
+            key = item[1:].strip()
+            data[key] = ''
         else:
-            data[key]+=ird.strip()
+            data[key] += ''.join(item.strip())
+
     return data
 
 
 def reverse_complementary(seq):
+
     complements = {"A":"T", "T":"A", "G":"C", "C":"G"}
+
     return ''.join([complements[c] for c in reversed(seq)])
 
     
 def reverse_palindromes(seq):
-    results = []
 
+    results = []
     for i in range(len(seq)):
         for j in range(4,13):
             if i+j>len(seq):
                 continue
             if seq[i:i+j] == reverse_complementary(seq[i:i+j]):
                 results.append((i+1,j))
+
     return results
 
 
 if __name__ == '__main__':
 
-    small_dataset = "./test.txt"
-    large_dataset = "./rosalind_revp.txt"
+    import sys
 
-    result = reverse_palindromes(parse_fasta(large_dataset).values()[0])
+    raw_data = Read_File()
+    data = Parse_FASTA(raw_data)
+    result = reverse_palindromes(data.values()[0])
     
     fw = open("./rosalind_revp.output.txt","w")
     fw.write("\n".join([" ".join(map(str,r)) for r in result]))
