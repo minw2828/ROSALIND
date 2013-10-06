55b3222c6b63dbac3c4dbcd42a494a822cbf2861
Sanyk28 <san-heng-yi-shu@163.com>
2013-10-07 01:20:04 +1100

    modify PPER/PPER.py

--- PPER/PPER.py
+++ PPER/PPER.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+
+
 '''
 Author:
 
@@ -5,30 +8,43 @@ Sanyk28 (san-heng-yi-shu@163.com)
 
 Date created:
 
-2 June 2013
+17 June 2013
 
 Rosalind problem:
 
-Given: A DNA string s of length at most 1000 nt.
+Partial Permutations
 
-Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s.
+Given: Positive integers n and k such that 100>=n>0 and 10>=k>0.
+
+Return: The total number of partial permutations P(n,k), modulo 1,000,000.
 
 Usage:
 
-python DNA.py [Input File]
+python PPER.py [Input File]
 
 '''
 
 
-from math import factorial
+def Read_File():
+
+    input_file = sys.argv[-1]
+    f = open(input_file)
+    raw_input = f.readline()
+    f.close()
+
+    return raw_input
+
 
 def partial_permutations(n,k):
     return factorial(n)/factorial(n-k)%1000000
+
     
 if __name__ == '__main__':
 
-    rd = open("./rosalind_pper.txt").read()
-    n = int(rd.split()[0])
-    k = int(rd.split()[1])
+    import sys
+    from math import factorial
+
+    raw_input = Read_File()
+    n,k = map(int,raw_input.split(' '))
+
     print partial_permutations(n,k)
-    
